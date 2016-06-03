dddb89b0ec2135f94a9860d7beea2bd72675fd6d
arnaudbey <arnaud.bey@gmail.com>
2016-06-03 11:59:43 +0200

    WIP combos (js)

--- src/MagicWordBundle/Resources/public/js/play/combo.js
+++ src/MagicWordBundle/Resources/public/js/play/combo.js
@@ -13,59 +13,33 @@ var combo = {
 		}
 	},
 
-	checkCurrentCombos: function(){
+	saveOrDestroy: function(){
 		for (var i = 0; i < this.currentCombos.length; i++) {
 			var currentCombo = this.currentCombos[i];
-			if ($.inArray(currentCombo, this.newIds) == -1) {
-				// le combo est fini.
-				// voire pour le stocker ou pas.
-			}
-
-		}
-
-		/*
-		for (var i = 0; i < roundJSON.combos.length; i++) {
-
-
-			var comboToCheck = roundJSON.combos[i];
-			var length = comboToCheck.length;
-			var number = comboToCheck.number;
-			var found = 0;
-
-			for (var i = 0; i < this.currentCombos.length; i++) {
-				var currentCombo = this.currentCombos[i];
-				if (currentCombo.count >= length) {
-					found++;
+			// cas où le lemme du combo n'est pas dans les lemmes courants
+			if ($.inArray(currentCombo.lemmaId, this.newIds) == -1) {
+				// si il a la taille requise on le sauvegarde
+				if(currentCombo.count >= this.minLengthTosave){
+					this.savedCombos.push(currentCombo);
 				}
+				// et dans tous les cas on l'enlève des combo courants
+				this.currentCombos.splice(i, 1);
 			}
-
-			if (found >= number) {
-
-
 		}
-		*/
 	},
 
-	delete: function(){
-
-	},
-
-	createOrIncrementCombo: function(){
-		console.log(this.newIds);
+	createOrIncrement: function(){
 		for (var i = 0; i < this.newIds.length; i++) {
 			var newId = this.newIds[i];
-			console.log("traitement de "+newId );
 			var found = false;
 			for (var j = 0; j < this.currentCombos.length; j++) {
 				var currentCombo = this.currentCombos[j];
 				if (currentCombo.lemmaId == newId) {
 					currentCombo.count++;
-					console.log("increment combo pour lemma "+newId+" : "+currentCombo.count);
 					found = true;
 				}
 			}
 			if(!found){
-				console.log("creation combo pour lemma "+newId+" : 1");
 				this.currentCombos.push({lemmaId:newId, count:1});
 			}
 		}
@@ -74,10 +48,8 @@ var combo = {
 	handleNewInflection: function(inflection){
 		var ids = gridJSON.inflections[inflection.toLowerCase()].lemmaIds;
 		this.newIds = ids;
-
-		this.createOrIncrementCombo();
-
-		this.checkCurrentCombos();
+		this.createOrIncrement();
+		this.saveOrDestroy();
 
 		return;
 	}
