c1dffd0d1feb01577af4ecd45655d4fd54cc0ad6
arnaudbey <arnaud.bey@gmail.com>
2016-06-03 14:04:04 +0200

    WIP combo JS

--- src/MagicWordBundle/Resources/public/js/play/combo.js
+++ src/MagicWordBundle/Resources/public/js/play/combo.js
@@ -13,9 +13,11 @@ var combo = {
 		}
 	},
 
-	saveOrDestroy: function(){
+	saveOrDestroy: function(callback){
+
 		for (var i = 0; i < this.currentCombos.length; i++) {
 			var currentCombo = this.currentCombos[i];
+
 			// cas où le lemme du combo n'est pas dans les lemmes courants
 			if ($.inArray(currentCombo.lemmaId, this.newIds) == -1) {
 				// si il a la taille requise on le sauvegarde
@@ -23,12 +25,16 @@ var combo = {
 					this.savedCombos.push(currentCombo);
 				}
 				// et dans tous les cas on l'enlève des combo courants
-				this.currentCombos.splice(i, 1);
+				for( j=this.currentCombos.length-1; j>=0; j--) {
+    				if( this.currentCombos[j].lemmaId == currentCombo.lemmaId) this.currentCombos.splice(j,1);
+				}
+				console.log("still a removing pb of current combos");
 			}
 		}
+		callback();
 	},
 
-	createOrIncrement: function(){
+	createOrIncrement: function(callback){
 		for (var i = 0; i < this.newIds.length; i++) {
 			var newId = this.newIds[i];
 			var found = false;
@@ -43,13 +49,41 @@ var combo = {
 				this.currentCombos.push({lemmaId:newId, count:1});
 			}
 		}
+		callback();
+	},
+
+	checkObjectives: function(){
+		for (var i = 0; i < roundJSON.combos.length; i++) {
+            var objective = roundJSON.combos[i];
+			var effectiveCount = 0;
+			for (var j = 0; j < this.currentCombos.length; j++) {
+				var currentCombo = this.currentCombos[j];
+				if (currentCombo.count >= objective.length) {
+					effectiveCount++;
+				}
+			}
+
+			for (var j = 0; j < this.savedCombos.length; j++) {
+				var currentCombo = this.savedCombos[j];
+				if (currentCombo.count >= objective.length) {
+					effectiveCount++;
+				}
+			}
+
+			if (effectiveCount >= objective.number){
+				$("#objective-combo-"+objective.id).addClass("list-group-item-success");
+			}
+
+		}
 	},
 
 	handleNewInflection: function(inflection){
-		var ids = gridJSON.inflections[inflection.toLowerCase()].lemmaIds;
-		this.newIds = ids;
-		this.createOrIncrement();
-		this.saveOrDestroy();
+		this.newIds = gridJSON.inflections[inflection.toLowerCase()].lemmaIds;
+		this.createOrIncrement(function(){
+			combo.saveOrDestroy(function(){
+				combo.checkObjectives();
+			});
+		});
 
 		return;
 	}
