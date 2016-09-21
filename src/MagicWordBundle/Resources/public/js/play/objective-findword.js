0872a6416b3e269273739a6d37a9076dff26d59f
arnaudbey <arnaud.bey@gmail.com>
2016-09-21 14:14:45 +0200

    valid findword objective if proposed form shares lemma with objective

--- src/MagicWordBundle/Resources/public/js/play/objective-findword.js
+++ src/MagicWordBundle/Resources/public/js/play/objective-findword.js
@@ -1,12 +1,21 @@
 var findword = {
     inWordsToFound: function(inflection){
-        if (roundJSON.findWords.hasOwnProperty(inflection.toLowerCase())){
-            var objectiveId = roundJSON.findWords[inflection.toLowerCase()].id;
-
-            objectives.considerAsDone(objectiveId);
-            this.appendWord(objectiveId, inflection);
-            activity.sendObjectiveDone(objectiveId);
+        var inflectionLower = inflection.toLowerCase();
+        if (roundJSON.findWords.hasOwnProperty(inflectionLower)){
+            this.found(roundJSON.findWords[inflectionLower], inflection);
             return true;
+        } else {
+            var lemmaIds = gridJSON.inflections[inflectionLower].lemmaIds;
+            for (var i = 0; i < lemmaIds.length; i++) {
+                for (var key in roundJSON.findWords) {
+                    if (!roundJSON.findWords.hasOwnProperty(key)) continue;
+                    var findword = roundJSON.findWords[key];
+                    if($.inArray(lemmaIds[i],findword.lemmaIds) != -1 ){
+                        this.found(findword, inflection);
+                        break;
+                    }
+                }
+            }
         }
 
         return false;
@@ -15,5 +24,11 @@ var findword = {
     appendWord: function(objectiveId, inflection){
         var str = ' ('+ inflection +')';
         $(".objective-"+objectiveId).append(str.toUpperCase());
+    },
+
+    found: function(objective, inflection){
+        objectives.considerAsDone(objective.id);
+        this.appendWord(objective.id, inflection);
+        activity.sendObjectiveDone(objective.id);
     }
 }
