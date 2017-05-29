10773f9e4650ab3fef5e37383dcc460b51f4feea
arnaudbey <arnaud.bey@gmail.com>
2017-05-29 15:58:13 +0200

    sound mute parameter is now effective

--- src/MagicWordBundle/Resources/public/js/play/sound.js
+++ src/MagicWordBundle/Resources/public/js/play/sound.js
@@ -1,5 +1,4 @@
 var sound = {
-    switchSelector: "input#mute-sound",
     rightWord: null,
     waitingStart: null,
     addLetter: null,
@@ -8,20 +7,27 @@ var sound = {
     objectiveDone: null,
     objectiveProgress: null,
     removeLetter: null,
-    disabled: true,
+    disabled: false,
 
     init: function(play){
-        this.rightWord = new Howl({src: [assetsBaseDir+'/audio/41381__sandyrb__db-bell-shot-001.mp3']});
-        this.waitingStart = new Howl({src: [assetsBaseDir+'/audio/49306__fossa11__drumroll.mp3']});
-        this.wrongForm = new Howl({src: [assetsBaseDir+'/audio/142608__autistic-lucario__error.mp3']});
-        this.notime = new Howl({src: [assetsBaseDir+'/audio/82523__zgump__bulgarian-gong.mp3']});
-        this.objectiveDone = new Howl({src: [assetsBaseDir+'/audio/165492__chripei__victory-cry-reverb-1.mp3']});
-        this.objectiveProgress = new Howl({src: [assetsBaseDir+'/audio/320655__rhodesmas__level-up-01.mp3']});
-        this.addLetter = new Howl({src: [assetsBaseDir+'/audio/212527__taira-komori__pushing-enter-key.mp3']});
-        this.removeLetter = new Howl({src: [assetsBaseDir+'/audio/360602__cabled-mess__typewriter-snippet-02.mp3']});
+        if ($("#mute-sound").val() != "true") {
+            this.rightWord = new Howl({src: [assetsBaseDir+'/audio/41381__sandyrb__db-bell-shot-001.mp3']});
+            this.waitingStart = new Howl({src: [assetsBaseDir+'/audio/49306__fossa11__drumroll.mp3']});
+            this.wrongForm = new Howl({src: [assetsBaseDir+'/audio/142608__autistic-lucario__error.mp3']});
+            this.notime = new Howl({src: [assetsBaseDir+'/audio/82523__zgump__bulgarian-gong.mp3']});
+            this.objectiveDone = new Howl({src: [assetsBaseDir+'/audio/165492__chripei__victory-cry-reverb-1.mp3']});
+            this.objectiveProgress = new Howl({src: [assetsBaseDir+'/audio/320655__rhodesmas__level-up-01.mp3']});
+            this.addLetter = new Howl({src: [assetsBaseDir+'/audio/212527__taira-komori__pushing-enter-key.mp3']});
+            this.removeLetter = new Howl({src: [assetsBaseDir+'/audio/360602__cabled-mess__typewriter-snippet-02.mp3']});
+        } else {
+            this.disabled = true;
+        }
+
     },
 
     play: function(s){
-        s.play();
+        if (this.disabled == false) {
+            s.play();
+        }
     }
 };
